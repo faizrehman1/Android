5d08eb617e77f3784c12ae72dcb8117c69789b4c
Kamran Ali <kamiali63@gmail.com>
2016-02-05 12:00:34 +0500

    Added new Project and some Modifications

---
+++ PicPasswordApp/app/src/main/java/com/android/picpasswordapp/PointCollector.java
@@ -0,0 +1,12 @@
+package com.android.picpasswordapp;
+
+import android.graphics.Point;
+
+import java.util.List;
+
+/**
+ * Created by Kamran ALi on 2/4/2016.
+ */
+public interface PointCollector {
+    public void pointListener(List<Point> points);
+}
---
+++ PicPasswordApp/app/src/main/java/com/android/picpasswordapp/TextViewActivity.java
@@ -0,0 +1,8 @@
+package com.android.picpasswordapp;
+
+/**
+ * Created by Kamran ALi on 2/4/2016.
+ */
+public class TextViewActivity {
+
+}
