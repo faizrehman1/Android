14d99ce214940be507a11661201ab36b5e1ac34b
Kamran Ali <kamiali63@gmail.com>
2016-02-08 12:21:40 +0500

    Rough Projects are deleted !

---
+++ PicPassApp/app/src/main/java/com/android/picpassapp/PointCollector.java
@@ -0,0 +1,12 @@
+package com.android.picpassapp;
+
+import android.graphics.Point;
+
+import java.util.List;
+
+/**
+ * Created by Kamran ALi on 2/5/2016.
+ */
+public interface PointCollector {
+    public void pointListener(List<Point> points);
+}
