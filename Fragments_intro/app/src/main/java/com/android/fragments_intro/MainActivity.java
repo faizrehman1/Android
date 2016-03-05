98336d38e57c19978fc815626af09eb8eeec9e5c
Kamran Ali <kamiali63@gmail.com>
2016-03-05 23:53:42 +0500

    New Files added``

--- Fragments_intro/app/src/main/java/com/android/fragments_intro/MainActivity.java
+++ Fragments_intro/app/src/main/java/com/android/fragments_intro/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.fragments_intro;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.List,new Fragment_list()).commit();
+
+        fragmentManager.beginTransaction().add(R.id.fragmentList,new CourseListFragment()).commit();
+
+
+
     }
 }
